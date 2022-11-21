bd19be25a448e88e5839a8c5a1509b9b3edcd2a4
Usuario <Usuario@CPD02-M03>
2022-11-21 08:31:52 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_SQLITE/CRUD_SQLITE/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CRUD_SQLITE
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
